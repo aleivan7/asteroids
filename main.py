5c05b56f83e408d511c9fb8acea607209abcda1e
Alejandro Carlo <alejandro.carlo77@gmail.com>
2026-06-30 00:21:19 -0500

    CH3: player is finished from asteroids lesson

--- main.py
+++ main.py
@@ -8,22 +8,38 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0.0
     clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("black")
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
-        player.draw(screen)
 
 
 if __name__ == "__main__":
