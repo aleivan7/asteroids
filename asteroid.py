de591f2e9334dc034acdeb8ce561b2e68f443ba5
Alejandro Carlo <alejandro.carlo77@gmail.com>
2026-07-04 10:44:35 -0500

    CH4: Added splitting of circles and removed gitignore

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+
+        angle = random.uniform(20, 50)
+        velocity_1 = self.velocity.rotate(angle)
+        velocity_2 = self.velocity.rotate(-angle)
+
+        new_angle = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_angle)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_angle)
+
+        asteroid_1.velocity = velocity_1 * 1.2
+        asteroid_2.velocity = velocity_2 * 1.2
